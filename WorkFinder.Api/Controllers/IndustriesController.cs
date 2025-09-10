051aa13dbbf708afbbce9cf1651a88ae50715366
talraza <talha.raza.1904@gmail.com>
2025-09-10 20:37:40 +0500

    GetIndustries end point

---
+++ WorkFinder.Api/Controllers/IndustriesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkFinder.ServiceContracts;
+using WorkFinder.ServiceContracts.DTOs.Industry;
+using WorkFinder.ServiceContracts.DTOs.Skill;
+using WorkFinder.Services;
+
+namespace WorkFinder.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class IndustriesController : ControllerBase
+    {
+        private readonly IIndustryService _industryService;
+
+        public IndustriesController(IIndustryService industryService)
+        {
+            _industryService = industryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IndustryResponseDto>> GetIndustries()
+        {
+            return Ok(await _industryService.GetIndustries());
+        }
+    }
+}
